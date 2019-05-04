ee7c05e336dbba9670e4ac5baad068c14aeac767
Benjamin Zamora <zsamora@ug.uchile.cl>
2019-05-04 19:07:13 -0400

    Configuraciones en Main

--- src/rTree/Config.java
+++
@@ -1,7 +0,0 @@
-package rTree;
-
-public final class Config {
-    public final static int MAX_M = 160;
-    public final static int MIN_M = (int) (MAX_M * 0.4);
-    public static int DISK_ACCESSES = 0;
-}
